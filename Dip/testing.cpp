108b62b792e925354b56be6da45d5f5acfb173c9
Ananasiig <zshen558@gmail.com>
2024-12-18 00:23:06 +0300

    Хи-квадрат

---
+++ Dip/testing.cpp
@@ -0,0 +1,20 @@
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include "2igamc.h"
+
+using namespace std;
+
+int main()
+{
+    double G = 0.1;
+  //  for (int i = 0; i < 100; i++) {
+      //  cout << tgamma(i*G)<<"\n";
+   // }
+    cout << gamain(1, 0.4, 1);
+}
