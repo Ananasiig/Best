1f87b189bada2855296f9668175bce7669b103ec
Ananasiig <zshen558@gmail.com>
2024-12-18 14:44:22 +0300

    +1 тест

---
+++ Dip/mduonis.h
@@ -0,0 +1,30 @@
+#pragma once
+
+double mdu(int l, string x) {
+	//”—Ћќ¬»я ѕ–»ћ≈Ќ≈Ќ»я: m >=50, m2_l >= 10
+
+
+	int m, u = pow(2, l);
+	m = x.length() / l;
+	map<string, int> v;
+	string sub;
+	double svalue = 0, m2_l = 1.0*m/u;
+
+	//частоты l-фрагментов
+	for (int t = 0; t < m; t++) {
+		sub = x.substr(l*t, l);
+		v[sub]++;
+	}
+
+	//вычисление статистики
+	for (const auto& element : v) {
+		svalue += 1.0 * pow(element.second - m2_l, 2) / m2_l;
+//		cout << element.second << ' ' << m2_l << '\n';
+	}
+//	cout << u << ' ' << v.size() << '\n';
+	svalue += 1.0 * (u - v.size()) * m2_l;
+//	cout << svalue << '\n';
+
+	//возвращаем P-значение
+	return gamain(svalue/2, 0.5*l, 1);
+}
--- Dip/testing.cpp
+++ Dip/testing.cpp
@@ -7,14 +7,19 @@
 #include <cstdlib>
 #include <ctime>
 #include "2igamc.h"
+#include "mduonis.h"
 
 using namespace std;
 
 int main()
 {
     double G = 0.1;
-  //  for (int i = 0; i < 100; i++) {
-      //  cout << tgamma(i*G)<<"\n";
-   // }
-    cout << gamain(1, 0.4, 1);
+   
+ //   cout << gamain(1, 0.4, 1);
+    string s = "11111000000111110000";
+    for (int i = 0; i < 10; i++) {
+        s += s;
+    }
+
+    cout << mdu(6, s);
 }
